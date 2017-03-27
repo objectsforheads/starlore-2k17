7ecff51270dbcb640c85535c876762481b2a4975
objectsforheads <dev@objectsforheads.com>
2017-03-26 23:30:25 -0700

    changes file naming pattern

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import startScreen from '@/layouts/startScreen';
+import start from '@/screens/start';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'startScreen',
-      component: startScreen,
+      name: 'start',
+      component: start,
     },
   ],
 });
