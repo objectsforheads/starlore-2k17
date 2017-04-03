92389c89a61d765663c1b257fa3e01e1702455d3
objectsforheads <dev@objectsforheads.com>
2017-04-02 19:32:20 -0700

    refactors cli input processing

--- src/components/cli.vue
+++ src/components/cli.vue
@@ -171,135 +171,112 @@ export default {
 
       if (cli.matchedCommands) {
         let action = cli.matchedCommands;
-        let output = (' ' + action.success.output).slice(1);
 
-        if (qualifyState(action.qualifier, cli.$store)) {
-          let wordOrder = [];
-
-          let verb = action.verbs.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-              wordOrder.push(command.indexOf(b));
-            }
-            return a;
-          }, null)
-          let noun = action.nouns.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-              wordOrder.push(command.indexOf(b));
-            }
-            return a;
-          }, null)
-
-          // Make sure words were entered in verb -> noun order
-          if (wordOrder[0] > wordOrder[1]) {
-            return cli.outputToCLI(
-              'Dad',
-              'Sorry, Scout, we don\'t speak in tongues in this household!'
-            );
-          }
-
-          // Parse the output if necessary
-          if ( /{.*?}/.test(action.success.output) ) {
-            output = output.replace('{verb}', verb.toUpperCase());
-            output = output.replace('{noun}', noun.toUpperCase());
-          }
-
-          return cli.outputToCLI(
-            action.success.outputter,
-            output,
-            action.resolution);
-        } else {
-          let wordOrder = [];
-          let verb = action.verbs.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-              wordOrder.push(command.indexOf(b));
-            }
-            return a;
-          }, null)
-          let noun = action.nouns.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-              wordOrder.push(command.indexOf(b));
-            }
-            return a;
-          }, null)
-
-          output = (' ' + action.error.qualifier.output).slice(1);
-          if ( /{.*?}/.test(output) ) {
-            output = output.replace('{verb}', verb.toUpperCase());
-            output = output.replace('{noun}', noun.toUpperCase());
-          }
+        // Return an error if we're not in the right game state to input this command
+        if (qualifyState(action.qualifier, cli.$store) === false) {
           return cli.outputToCLI(
             action.error.qualifier.outputter,
-            output);
+            parseOutput(action, action.error.qualifier.output)
+          );
         }
 
-        function qualifyState(reqs, current) {
-          // Accumulate qualifications to reduce later
-          let qualifications = [];
-          // Loop recursively through each req in the reqs object
-          // Comparing it to the current state object and checking for a match
-          for (var req in reqs) {
-            if (typeof reqs[req] === 'object' && typeof current[req] === 'object') {
-              qualifications = qualifications.concat(qualifyState(reqs[req], current[req]))
-            } else {
-              qualifications.push(reqs[req] === current[req]);
-            }
+        // Return an error if the words weren't input in verb -> noun order
+        let wordOrder = [];
+        action.verbs.forEach(function(verb) {
+          if (command.indexOf(verb) > -1) {
+            wordOrder.push(command.indexOf(verb));
           }
+        })
+        action.nouns.forEach(function(noun) {
+          if (command.indexOf(noun) > -1) {
+            wordOrder.push(command.indexOf(noun));
+          }
+        })
 
-          // Reduce qualifications to a boolean
-          return qualifications.reduce(function(a, b) {
-            if (b === false || typeof b !== 'boolean') {
-              a = false;
-            }
-            return a;
-          }, true)
+        if (wordOrder[0] > wordOrder[1]) {
+          return cli.outputToCLI(
+            'Dad',
+            'Sorry, Scout, we don\'t speak in tongues in this household!'
+          );
         }
+
+        // All clear, return the command and resolver
+        return cli.outputToCLI(
+          action.success.outputter,
+          parseOutput(action, action.success.output),
+          action.resolution);
       }
-      else if (cli.verbMatchedCommands) {
-        let action = cli.verbMatchedCommands;
-        let output = (' ' + action.error.verb.output).slice(1);
 
-        if ( /{.*?}/.test(output) ) {
-          let verb = action.verbs.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-            }
-            return a;
-          }, null)
+      if (cli.verbMatchedCommands || cli.nounMatchedCommands) {
+        // If we only matched a noun or a verb, return a specific error if it exists
+        let outputter = 'Dad';
+        let output = 'By Jove, I think you\'re onto something, Scout!';
 
-          output = output.replace('{verb}', verb.toUpperCase());
+        if (cli.verbMatchedCommands && cli.verbMatchedCommands.error) {
+          let action = cli.verbMatchedCommands;
+          outputter = action.error.outputter;
+          output = parseOutput(action, action.error.verb.output);
+        }
+        if (cli.nounMatchedCommands && cli.nounMatchedCommands.error) {
+          let action = cli.nounMatchedCommands;
+          outputter = action.error.outputter;
+          output = parsedOutput(action, action.error.noun.output);
         }
 
         return cli.outputToCLI(
-          action.error.verb.outputter,
-          output);
+          outputter,
+          output
+        );
       }
-      else if (cli.nounMatchedCommands) {
-        let action = cli.nounMatchedCommands;
-        let output = (' ' + action.error.noun.output).slice(1);
 
-        if ( /{.*?}/.test(output) ) {
-          let noun = action.nouns.reduce(function(a, b) {
-            if (command.indexOf(b) > -1) {
-              a = b;
-            }
-            return a;
-          }, null)
+      // Return a generic error if there is no specific error to be called
+      return cli.outputToCLI(
+        'event',
+        'Dad looks at you funny'
+      )
 
-          output = output.replace('{noun}', noun.toUpperCase());
+      function qualifyState(reqs, current) {
+        // Accumulate qualifications to reduce later
+        let qualifications = [];
+        // Loop recursively through each req in the reqs object
+        // Comparing it to the current state object and checking for a match
+        for (var req in reqs) {
+          if (typeof reqs[req] === 'object' && typeof current[req] === 'object') {
+            qualifications = qualifications.concat(qualifyState(reqs[req], current[req]))
+          } else {
+            qualifications.push(reqs[req] === current[req]);
+          }
         }
 
-        return cli.outputToCLI(
-          action.error.verb.outputter,
-          output);
-      } else {
-        return cli.outputToCLI(
-          'event',
-          'Dad looks at you funny'
-        )
+        // Reduce qualifications to a boolean
+        return qualifications.reduce(function(a, b) {
+          if (b === false || typeof b !== 'boolean') {
+            a = false;
+          }
+          return a;
+        }, true)
+      }
+
+      function parseOutput(action, output) {
+        // Find matched noun and verb, if any
+        let verb = action.verbs.reduce(function(a, b) {
+          if (command.indexOf(b) > -1) {
+            a = b;
+          }
+          return a;
+        }, null)
+        let noun = action.nouns.reduce(function(a, b) {
+          if (command.indexOf(b) > -1) {
+            a = b;
+          }
+          return a;
+        }, null)
+
+        // Clone the output for modifying
+        let parsed = (' ' + output).slice(1);
+        if (verb) { parsed = parsed.replace('{verb}', verb.toUpperCase()); }
+        if (noun) { parsed = parsed.replace('{noun}', noun.toUpperCase()); }
+        return parsed;
       }
     },
     outputToCLI: function(outputter, output, resolution) {
