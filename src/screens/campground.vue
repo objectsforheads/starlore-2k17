372d04e07f31bf7835185929cd81ea4aa7e9dde9
objectsforheads <dev@objectsforheads.com>
2017-04-04 20:37:32 -0700

    sets up campground scene and fire sprite reactivity

--- src/screens/campground.vue
+++ src/screens/campground.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="campground">
     <div class="foreground">
-      <div class="sprite">
-        <div class="fire"></div>
-      </div>
+      <div class="fire sprite" v-bind:class="isTutorial"></div>
     </div>
     <game-cli></game-cli>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+  export default {
+    name: 'campground',
+    computed: {
+      isTutorial: function() {
+        if (this.$store.state.isTutorial) {
+          return false;
+        }
+        return 'lit';
+      }
+    }
+  }
+</script>
 
 <style scoped lang="scss">
   // HACK really need a better way to import variables
@@ -20,5 +33,17 @@
   .campground {
     width: 100%;
     height: 100%;
+  }
+
+  .foreground {
+    height: 100%;
+    max-height: calc(100% - 250rem); // CLI height
+  }
+
+  .fire {
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
   }
 </style>
