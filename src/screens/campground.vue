7f6ef5fa8a327cbe5984092d91b744c7f39ef236
objectsforheads <dev@objectsforheads.com>
2017-04-05 19:01:21 -0700

    adds campground and its lighting animations/triggers

--- src/screens/campground.vue
+++ src/screens/campground.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="campground">
-    <div class="foreground">
-      <div class="fire sprite" v-bind:class="isTutorial"></div>
+    <div class="foreground" v-bind:class="isTutorial">
+      <div class="mountains sprite"></div>
+      <div class="land sprite">
+        <div class="land--1"></div>
+        <div class="land--2"></div>
+        <div class="land--3"></div>
+        <div class="land--4"></div>
+        <div class="land--5"></div>
+      </div>
+      <div class="fire sprite"></div>
     </div>
     <game-cli></game-cli>
   </div>
@@ -40,10 +48,76 @@
     max-height: calc(100% - 250rem); // CLI height
   }
 
+  .mountains {
+    position: absolute;
+    z-index: 0;
+
+    position: absolute;
+    z-index: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 35em;
+
+    opacity: 0;
+  }
+
+  .land {
+    position: absolute;
+    z-index: 1;
+    bottom: -45em;
+    left: 50%;
+    transform: translateX(calc(-50% - 30em));
+
+    [class*="land--"] {
+      opacity: 0;
+    }
+  }
+
   .fire {
     position: absolute;
+    z-index: 2;
     bottom: 0;
     left: 50%;
     transform: translateX(-50%);
+
+    animation: fireStart steps(1) 1000ms infinite alternate;
+  }
+
+  .lit {
+    .fire {
+      animation: fireGoing steps(1) 450ms infinite alternate;
+    }
+
+    .land {
+      [class*="land--"] {
+        animation: fadeIn 180ms;
+        animation-fill-mode: both;
+      }
+
+      .land--1 {
+        animation-delay: 30ms;
+      }
+      .land--2 {
+        animation-delay: 120ms;
+      }
+      .land--3 {
+        animation-duration: 210ms;
+        animation-delay: 210ms;
+      }
+      .land--4 {
+        animation-duration: 270ms;
+        animation-delay: 360ms;
+      }
+      .land--5 {
+        animation-duration: 270ms;
+        animation-delay: 450ms;
+      }
+    }
+
+    .mountains {
+      animation: fadeIn 360ms;
+      animation-fill-mode: both;
+      animation-delay: 540ms;
+    }
   }
 </style>
